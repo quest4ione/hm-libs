e74c0d1db6c3c8a9853d070109145c14a207aa18
quest4ione <196376222+quest4ione@users.noreply.github.com>
2025-04-05 14:26:42 +0200

    added basic permission check

--- db.js
+++ db.js
@@ -1,5 +1,15 @@
 function(C, A) {
+  let L = #fs.qst.lib();
+
   let DB = {};
+
+  if (C.calling_script) {
+    if (C.calling_script.split(".")[0] != C.this_script.split(".")[0]) {
+      return L.F.res({ status: 403, msg: "nope." });
+    }
+  } else if (C.caller != C.this_script.split(".")[0] || C.is_scriptor) {
+    return L.F.res({ status: 403, msg: "not happening" });
+  }
 
   return DB;
 }
