b5d47f29dddafab47f7ed16721aedaf932ebf27f
quest4ione <196376222+quest4ione@users.noreply.github.com>
2025-04-10 16:34:05 +0200

    Added option/result try integration (and cleaned up some comments)

--- db.js
+++ db.js
@@ -7,37 +7,40 @@ function(C, A) {
 
   let DB = {
     __get(id) {
-      if (!(id in #G.cache)) {
-        let res = L.R.rslt.catch_into_result(() => #db.f({_id: id}));
-        if (!res.ok) {
-          return res;
+      return L.R.rslt.try_wrapper(() => L.R.optn.try_wrapper(() => {
+        // fetch the doc if it is uncached
+        if (!(id in #G.cache)) {
+          let res = L.R.rslt.catch_into_result(() => #db.f({ _id: id })).try();
+          #G.cache[id] = res.first();
         }
-        #G.cache[id] = res.unwrap().first();
-      }
 
-      if (#G.cache[id] === null) {
-        return L.R.rslt.ok(L.R.optn.none());
-      }
-      return L.R.rslt.ok(L.R.optn.some(#G.cache[id]));
+        // return the doc or none if it is null
+        if (#G.cache[id] === null) {
+          return L.R.rslt.ok(L.R.optn.none());
+        }
+        return L.R.rslt.ok(L.R.optn.some(#G.cache[id]));
+      }));
     },
 
     __store(id, doc) {
-      // reject update query
-      if (Object.keys(doc).some(k => k.startsWith("$"))) {
-        return L.R.rslt.err(L.R.custom_err("QstDbUpdateForbidden", "Update queries are not permitted here", {query: doc}));
-      }
+      return L.R.rslt.try_wrapper(() => {
+        // reject update query for caching
+        if (Object.keys(doc).some(k => k.startsWith("$"))) {
+          return L.R.rslt.err(L.R.custom_err("QstDbUpdateForbidden", "Update queries are not permitted here (yet)", { query: doc }));
+        }
 
-      let res = L.R.rslt.catch_into_result(() => #db.us({_id: id}, doc));
-      if (!res.ok) {
-        return res;
-      }
-      res = res.unwrap()[0];
-      // account for update result having an ok == false (which probably never happens)
-      if (!res.ok) {
-        return L.R.rslt.err(L.R.custom_err("QstDbOperationFailed", "Db operation failed for unknown reasons", {response: res}));
-      }
-      #G.cache[id] = doc;
-      return L.R.rslt.ok();
+        let res = L.R.rslt.catch_into_result(() => #db.us({ _id: id }, doc)).try();
+        res = res[0]; // get the first upsert result as there is only one
+
+        // account for update result having an ok == false (which probably never happens)
+        if (!res.ok) {
+          return L.R.rslt.err(L.R.custom_err("QstDbOperationFailed", "Db operation failed for unknown reasons", { response: res }));
+        }
+
+        // cache the result
+        #G.cache[id] = doc;
+        return L.R.rslt.ok();
+      });
     },
   };
 
